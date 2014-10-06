101b7678e271b25f3a6cea62fcbbad56c9ee9fc5
Mark Barrasso <mark.barrasso@live.com>
2014-10-06 11:47:48 -0400

    created schievement class

---
+++ Source/AchieveScene.h
@@ -0,0 +1,13 @@
+//
+//  AchieveScene.h
+//  DojoDefense
+//
+//  Created by Mark on 10/6/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface AchieveScene : CCNode
+
+@end
---
+++ Source/AchieveScene.m
@@ -0,0 +1,13 @@
+//
+//  AchieveScene.m
+//  DojoDefense
+//
+//  Created by Mark on 10/6/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "AchieveScene.h"
+
+@implementation AchieveScene
+
+@end
